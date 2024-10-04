2186111b199e7ecf5812100bef3c517e0d632f50
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-04 17:16:01 +0300

    Initializing new project and doing first exercise

---
+++ Chapter1/NameAndAddress/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NameAndAddress
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is John Doe!");
+            Console.WriteLine("My address is Stepova Vul., bld. 3");
+            Console.ReadKey();
+        }
+    }
+}
