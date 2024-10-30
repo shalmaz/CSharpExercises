950b540188e5ecc019470c66f49229d0f96412de
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 16:49:40 +0200

    Exercise 4

---
+++ Chapter1/Exercise4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your age in months: {age * 12}");
+        }
+    }
+}
