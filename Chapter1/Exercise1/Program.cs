f5b691fed9ee90cdf7fb4048e24f94d299cb28f6
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 16:42:25 +0200

    Create new project for exercise 1 and solution for it

---
+++ Chapter1/Exercise1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is John Doe");
+            Console.WriteLine("My address vul. Zelena 13");
+        }
+    }
+}
