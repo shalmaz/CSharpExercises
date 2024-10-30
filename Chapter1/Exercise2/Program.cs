e8a326c1b4dad9527841d74a3a83cd02b9fcb79d
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 16:47:34 +0200

    Exercise 2

---
+++ Chapter1/Exercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Distance in furlongs: ");
+            int furlongs = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Distance in yards is {furlongs * 220}");
+        }
+    }
+}
