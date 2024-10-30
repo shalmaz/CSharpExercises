d7036ffaa857d2c0f10f8660c2ee361caa90cfec
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 16:54:10 +0200

    Exercise 6

---
+++ Chapter1/Exercise6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of light years: ");
+            double lightYears = double.Parse(Console.ReadLine());
+            double astronomicalUnits = LightYearsToAstronomicalUnits(lightYears);
+            Console.WriteLine($"{lightYears} light years = {astronomicalUnits} astronomical units");
+        }
+        static double LightYearsToAstronomicalUnits(double lightYears)
+        {
+            return lightYears * 63240;
+        }
+    }
+}
