84b27a9f82f29b3044d234be4e526644875abfda
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 18:03:45 +0200

    Exercise 1

---
+++ Chapter2/Exercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        const int ITF = 12;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your height in integer inches: ___\b\b\b");
+            int inch = int.Parse(Console.ReadLine());
+            int feet = inch/ITF;
+            int inch2 = inch%ITF;
+            Console.WriteLine($"Your height is {feet} feet and {inch2} inches");
+        }
+    }
+}
