9143d343a1258fadc9f3366597b63f4240d1b494
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 16:52:28 +0200

    Exercise 5

---
+++ Chapter1/Exercise5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a Celsius value: ");
+            int cel = int.Parse(Console.ReadLine());
+            double fahrenheit = CelsiusToFahrenheit(cel);
+            Console.WriteLine($"{cel} degrees Celsius is {fahrenheit} degrees Fahrenheit");
+        }
+        static double CelsiusToFahrenheit(int cel)
+        {
+            double fahrenheit = 1.8 * cel + 32.0;
+            return fahrenheit;
+        }
+    }
+}
