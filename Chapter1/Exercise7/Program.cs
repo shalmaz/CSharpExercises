6cb332deb5878ae5e0d03abfc32e2e9d9835f258
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 16:54:53 +0200

    Exercise 7

---
+++ Chapter1/Exercise7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of hours: ");
+            int hours = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of minutes: ");
+            int minutes = int.Parse(Console.ReadLine());
+            ShowTime(hours, minutes);
+        }
+        static void ShowTime(int hours, int minutes)
+        {
+            Console.WriteLine($"Time: {hours}:{minutes}");
+        }
+    }
+}
