e661e0df0a42fb1f29d352b4d966baf576a73afc
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-30 16:48:49 +0200

    Exercise 3

---
+++ Chapter1/Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreeBlindMice();
+            ThreeBlindMice();
+            SeeHowTheyRun();
+        }
+        static void ThreeBlindMice()
+        {
+            Console.WriteLine("Three blind mice");
+        }
+        static void SeeHowTheyRun()
+        {
+            Console.WriteLine("See how they run");
+            Console.WriteLine("See how they run");
+        }
+    }
+}
