cdf174e49ae029380f622cf415bb2a80ae56c81c
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-31 18:49:06 +0200

    Exercise 4

---
+++ Chapter2/Exercise4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        const int HID = 24;
+        const int MIH = 60;
+        const int SIM = 60;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of seconds: ");
+            long temp, sec = long.Parse(Console.ReadLine());
+            int secunds = (int)(sec % SIM);
+            temp = sec / SIM;
+            int minutes = (int)(temp % MIH);
+            temp /= MIH;
+            int hours = (int)(temp % HID);
+            temp /= HID;
+            int days = (int)temp;
+            Console.WriteLine($"{sec} seconds = {days} days, {hours} hours, {minutes} minutes, {secunds} seconds");
+        }
+    }
+}
