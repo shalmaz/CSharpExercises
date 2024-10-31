7e208ee0e99c9e71ebdb264fd2ec7c46d161dff9
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-31 21:39:27 +0200

    Exercise 7

---
+++ Chapter2/Exercise7/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much liters of petrolium per 100km have used your car: ");
+            double literPerKM = double.Parse(Console.ReadLine());
+            double mpg = 62.14 / (literPerKM / 3.875);
+            Console.WriteLine($"Your car have used {mpg} mpg");
+        }
+    }
+}
