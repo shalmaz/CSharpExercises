94b35e57a7328ae2276c12d12c85a9fec15e365a
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-31 21:12:07 +0200

    Exercise 6

---
+++ Chapter2/Exercise6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much kilometers you have driven: ");
+            int kilometers = int.Parse(Console.ReadLine());
+            Console.Write("How many petrol in liters you've used: ");
+            int liters = int.Parse(Console.ReadLine());
+            double litersPerKilometers = (double)liters / kilometers * 100;
+            Console.WriteLine($"Your car has gotten {litersPerKilometers} liters per kilometer" );
+        }
+    }
+}
