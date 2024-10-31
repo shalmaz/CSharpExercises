995f7187f6e90b94bf16eaa49a3d9de4b529a00f
Ігор Чуркін <shalmazmail@gmail.com>
2024-10-31 18:58:52 +0200

    Exercise 5

---
+++ Chapter2/Exercise5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the world's population: ");
+            long worldPopulation = long.Parse(Console.ReadLine());
+            Console.Write("Enter the population of the US: ");
+            long usPopulation = long.Parse(Console.ReadLine());
+            double percent = (double)usPopulation / worldPopulation;
+            Console.WriteLine($"The population of the US is {percent*100}% of the world population.");
+        }
+    }
+}
